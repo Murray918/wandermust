567f9b1571b03f29cebe3f24a2ac784d8a8e2e00
saraharless <sara.e.harless@gmail.com>
2017-10-04 11:44:05 -0500

    add baselayout render to app.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WanderMust
 
-## mock ups for base layout and individual pages
+## mockups for base layout and individual pages
 ![mockup sketch](src/images/mockuppageone.jpg)
 
 ![mockup sketch](src/images/mockuppagetwo.jpg)
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
 
-class App extends Component {
+import React, { Component } from 'react';
+import '../styles/App.css';
+import BaseLayout from '../components/BaseLayout.js'
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-    
-      </div>
+      <BaseLayout/>
     );
   }
 }
-
-export default App;
