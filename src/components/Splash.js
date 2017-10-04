d7fad14e7accf6dc6af32ce6f2a545da7c475479
saraharless <sara.e.harless@gmail.com>
2017-10-04 14:11:26 -0500

    create routes in index, add folders for each component

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import arrowUpDown from '../components/arrowUpDown.js'
 
-export default class arrowUpDown extends Component {
-  
+export default class Splash extends Component {
+  render() {
+    return (
+      <p>65</p>
+    )
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import BaseLayout from './components/BaseLayout.js';
+import Splash from './components/Splash.js';
+import List from './containers/List.js';
+import Result from './containers/Result.js';
+import UserProfile from './containers/UserProfile.js';
+import Explore from './containers/Explore.js';
+import ExploreTemp from './containers/ExploreTemp.js';
+import ExploreCat from './containers/ExploreCat.js';
+
+ReactDOM.render(
+<BrowserRouter>
+<BaseLayout>
+  <Switch>
+    <Route exact path='/' component={Splash} />
+    <Route path='/list' component={List} />
+    <Route path='/result' component={Result} />
+    <Route path='/user' component={UserProfile} />
+    <Route path='/explore' component={Explore} />
+    <Route path='/exploretemperature' component={ExploreTemp} />
+    <Route path='/explorecategory' component={ExploreCat} />
+  </Switch>
+</BaseLayout>
+</BrowserRouter>
+
+,document.getElementById('root')
+);
 registerServiceWorker();
